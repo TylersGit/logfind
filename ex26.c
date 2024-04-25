8d3c78e400121862988e98be8f4dbbec8c51e907
root <root@localhost.localdomain>
2024-04-25 11:41:40 +0300

    Fixed invalid writes

--- ex26.c
+++ ex26.c
@@ -3,19 +3,22 @@
 #include "dbg.h"
 #include "logfind.h"
 
-char **read_logfind_dot_file(FILE *logfind_dot_file)
+char **read_logfind_dot_file(FILE *logfind_dot_file, int *number_of_lines)
 {
 
   char *rc = NULL;
   int true = 1;
-  char **files_to_search = malloc(sizeof(char *));
+  // I tried to initialize an array with a single poinnter. This 
+  // did not work. Only the first element was initialized properly, but
+  // I did not malloc enough memory for additional items. 
+  char **files_to_search = malloc(sizeof(char *) * MAX_FILES_TO_SEARCH);
   char **orig_address = files_to_search;
 
   rewind(logfind_dot_file);
 
   while (true)
   {
-    *files_to_search = malloc(sizeof(char) * MAX_DATA);
+    *files_to_search = malloc(MAX_DATA);
     rc = fgets(*files_to_search, MAX_DATA, logfind_dot_file);
 
     if (rc == NULL)
@@ -24,6 +27,7 @@ char **read_logfind_dot_file(FILE *logfind_dot_file)
     }
 
     files_to_search++;
+    (*number_of_lines)++;
   }
 
   return orig_address;
@@ -32,40 +36,45 @@ error:
     free(*files_to_search);
 }
 
-void cleanup(FILE *logfind_dot_file, char **file_list)
-{
-  int i = 0;
-  if (logfind_dot_file)
-    free(logfind_dot_file);
-  for (i = 0; file_list[i]; i++)
-  {
-    if (strcmp(file_list[i], "") == 0 || file_list[i] == NULL)
-    {
-      break;
-    }
-
-    if (file_list[i]) free(file_list[i]);
-  }
-}
+// void cleanup(FILE *logfind_dot_file, char **file_list, int *ptr_number_of_lines)
+// {
+//   int i = 0;
+//   if (logfind_dot_file)
+//     free(logfind_dot_file);
+//   for (i = 0; file_list[i]; i++)
+//   {
+//     if (strcmp(file_list[i], "") == 0 || file_list[i] == NULL)
+//     {
+//       break;
+//     }
+//     if (file_list[i]) free(file_list[i]);
+//   }
+//  if (ptr_number_of_lines)
+//  {
+//   free(ptr_number_of_lines);
+//  }
+// }
 
 int main(int argc, char *argv[])
 {
   FILE *logfind_dot_file = fopen(DOT_FILE, "r");
   check(logfind_dot_file != NULL, "Failed to load file.");
+  int number_of_lines = 0;
+  int *ptr_number_of_lines = &number_of_lines;
 
-  char **file_list = read_logfind_dot_file(logfind_dot_file);
+  char **file_list = read_logfind_dot_file(logfind_dot_file, ptr_number_of_lines);
 
   int i = 0;
-  for (i = 0; file_list[i]; i++)
+  for (i = 0; i < number_of_lines; i++)
   {
     if (strcmp(file_list[i], "") == 0 || file_list[i] == NULL)
       break;
     printf("String: %s \n", file_list[i]);
   }
 
-  cleanup(logfind_dot_file, file_list);
+  // cleanup(logfind_dot_file, file_list);
   return 0;
 error:
-  cleanup(logfind_dot_file, file_list);
+  // cleanup(logfind_dot_file, file_list);
   return -1;
 }
--- logfind.h
+++ logfind.h
@@ -1,5 +1,6 @@
 #define DOT_FILE ".logfind"
 #define MAX_DATA 512 
+#define MAX_FILES_TO_SEARCH 10
 
 // char **read_logfind_dot_file(FILE *logfind_dot_file);
 
